8b35dd06968f495bd042d3c3d836343a3f22e2e4
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-17 03:18:29 +0500

    try

---
+++ main.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber"
+    },
+    { message: "Enter second number", type: "number", name: "secondNumber"
+    },
+    {
+        massage: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//condition statement
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
